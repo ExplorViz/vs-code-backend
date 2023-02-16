04fc0acff2d19e1f58b81b951f2cc530e91f8701
Lennart Ideler <lennart.ideler@gmail.com>
2023-02-16 15:25:27 +0100

    copied and adjusted codebase for backend server from vscode-extension repo

---
+++ src/app.ts
@@ -0,0 +1,72 @@
+import express from 'express';
+import {Server} from 'socket.io';
+import http from 'http';
+import { IDEApiActions, IDEApiCall, IDEApiDest } from './types';
+
+const backend = express();
+const server = http.createServer(backend);
+const maxHttpBufferSize = 1e8;
+
+const port = 3000;
+const corsExplorVizHttp = 'http://localhost:4200'
+
+
+const io = new Server(server, {
+  maxHttpBufferSize: maxHttpBufferSize,
+  cors: {
+    origin: corsExplorVizHttp,
+    methods: ['GET', 'POST'],
+  },
+});
+
+
+console.log("Max http buffer size for Socket data: " + (maxHttpBufferSize / 1e6) + "mb")
+
+io.on('connection', (socket) => {
+  console.log('Backend Sockets established.');
+
+  socket.on(IDEApiDest.VizDo, (data: IDEApiCall) => {
+    // console.log("vizDo", data)
+    socket.broadcast.emit(IDEApiDest.VizDo, data);
+  });
+  socket.on(IDEApiDest.IDEDo, (data: IDEApiCall) => {
+    // console.log("ideDo", data)
+    socket.broadcast.emit(IDEApiDest.IDEDo, data);
+  });
+
+  socket.on(IDEApiActions.Refresh, () => {
+    const data: IDEApiCall = {action: IDEApiActions.GetVizData, data: [], fqn: "", meshId: "", occurrenceID: -1}
+    socket.emit(IDEApiDest.VizDo, data);
+    // console.log("ideDo")
+    
+  });
+
+  socket.on('vizDoubleClickOnMesh', (data) => {
+    console.log('vizDoubleClickOnMesh: ', data);
+  });
+
+
+  socket.on('disconnect', (reason) => {
+    console.error(`Socket disconnected, reason: ${reason}`);
+    // console.error(`Possible solution: Increase current maxHttpBufferSize of ` + (maxHttpBufferSize / 1e6) + "mb");
+  });
+
+});
+
+
+
+// backend.get('/', (req, res) => {
+//   console.log('/');
+//   res.send('Hello World!!');
+// });
+
+// backend.get('/testOne', (req, res) => {
+//   console.log('/ testOne');
+//   res.send('testOne');
+// });
+
+server.listen(port, () => {
+  console.log(`Example backend listening on port ${port}`);
+});
+
+export { backend, port, io };
---
+++ src/types.ts
@@ -0,0 +1,54 @@
+export enum IDEApiDest {
+    VizDo = "vizDo",
+    IDEDo = "ideDo",
+  }
+  
+  export enum IDEApiActions {
+    Refresh = "refresh",
+    SingleClickOnMesh = "singleClickOnMesh",
+    DoubleClickOnMesh = "doubleClickOnMesh",
+    ClickTimeline = "clickTimeLine",
+    GetVizData = "getVizData",
+    JumpToLocation = "jumpToLocation",
+    JumpToMonitoringClass = "jumpToMonitoringClass"
+  }
+  
+  export type IDEApiCall = {
+    action: IDEApiActions,
+    data: OrderTuple[],
+    meshId: string,
+    occurrenceID: number,
+    fqn: string
+  }
+  
+  export type ParentOrder = {
+    fqn: string,
+    childs: ParentOrder[],
+    meshId: string,
+  }
+  
+  export type OrderTuple = {
+    hierarchyModel: ParentOrder,
+    meshes: { meshNames: string[], meshIds: string[] }
+  }
+  
+  export type classMethod = {
+    name: string,
+    fqn: string,
+    lineString: string,
+    lineNumber: number,
+    // meshId: string,
+    // fileLocation: string,
+  }
+  
+  export type FoundationOccurrences = {
+    foundation: string,
+    occurrences: number[]
+  }
+  
+  
+  export type LocationFind = {
+    javaFiles: string[],
+    dirs: string[],
+    javaFile: string[]
+  }
