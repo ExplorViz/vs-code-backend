b2990df186fbed10b9ad6f73b94c30b42cfe2304
Alexander Krause <akr@informatik.uni-kiel.de>
2023-02-17 14:56:12 +0100

    qa refactoring and enable some log statements

--- src/app.ts
+++ src/app.ts
@@ -1,59 +1,63 @@
-import express from 'express';
-import {Server} from 'socket.io';
-import http from 'http';
-import { IDEApiActions, IDEApiCall, IDEApiDest } from './types';
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
+import { IDEApiActions, IDEApiCall, IDEApiDest } from "./types";
 
 const backend = express();
 const server = http.createServer(backend);
 const maxHttpBufferSize = 1e8;
 
 const port = 3000;
-const corsExplorVizHttp = 'http://localhost:4200'
-
+const corsExplorVizHttp = "http://localhost:4200";
 
 const io = new Server(server, {
   maxHttpBufferSize: maxHttpBufferSize,
   cors: {
     origin: corsExplorVizHttp,
-    methods: ['GET', 'POST'],
+    methods: ["GET", "POST"],
   },
 });
 
+console.log(
+  "Max http buffer size for Socket data: " + maxHttpBufferSize / 1e6 + "mb"
+);
 
-console.log("Max http buffer size for Socket data: " + (maxHttpBufferSize / 1e6) + "mb")
+io.on("connection", (socket) => {
+  //console.log('Backend Sockets established.');
 
-io.on('connection', (socket) => {
-  console.log('Backend Sockets established.');
+  console.log(`Socket ${socket.id} connected.`);
 
   socket.on(IDEApiDest.VizDo, (data: IDEApiCall) => {
-    // console.log("vizDo", data)
+    console.log("vizDo", data);
     socket.broadcast.emit(IDEApiDest.VizDo, data);
   });
   socket.on(IDEApiDest.IDEDo, (data: IDEApiCall) => {
-    // console.log("ideDo", data)
+    console.log("ideDo", data);
     socket.broadcast.emit(IDEApiDest.IDEDo, data);
   });
 
   socket.on(IDEApiActions.Refresh, () => {
-    const data: IDEApiCall = {action: IDEApiActions.GetVizData, data: [], fqn: "", meshId: "", occurrenceID: -1}
+    console.log(`refresh sent by ${socket.id}`);
+    const data: IDEApiCall = {
+      action: IDEApiActions.GetVizData,
+      data: [],
+      fqn: "",
+      meshId: "",
+      occurrenceID: -1,
+    };
     socket.emit(IDEApiDest.VizDo, data);
     // console.log("ideDo")
-    
   });
 
-  socket.on('vizDoubleClickOnMesh', (data) => {
-    console.log('vizDoubleClickOnMesh: ', data);
+  socket.on("vizDoubleClickOnMesh", (data) => {
+    console.log("vizDoubleClickOnMesh: ", data);
   });
 
-
-  socket.on('disconnect', (reason) => {
-    console.error(`Socket disconnected, reason: ${reason}`);
+  socket.on("disconnect", (reason) => {
+    console.error(`Socket ${socket.id} disconnected, reason: ${reason}`);
     // console.error(`Possible solution: Increase current maxHttpBufferSize of ` + (maxHttpBufferSize / 1e6) + "mb");
   });
-
 });
-
-
 
 // backend.get('/', (req, res) => {
 //   console.log('/');
@@ -66,7 +70,7 @@ io.on('connection', (socket) => {
 // });
 
 server.listen(port, () => {
-  console.log(`Example backend listening on port ${port}`);
+  console.log(`VS Code backend listening on port ${port}`);
 });
 
 export { backend, port, io };
